ab69b843ef92caaeb2b0dc7ab7b6a870b57cee8d
Ari Kishon <ariki@wix.com>
2021-12-17 19:41:07 +0200

    fix: fixed typo in SkyBox

--- src/Components/Utils/Skybox.tsx
+++ src/Components/Utils/Skybox.tsx
@@ -1,11 +1,8 @@
 import { useThree } from "@react-three/fiber";
-import { ColorRepresentation, CubeTextureLoader } from "three";
+import { CubeTextureLoader } from "three";
 import defaultSkybox from "../../Assets/SkyBoxes/default-skybox/default-skybox";
 
 export interface ISkyboxProps{
-    color: [color: ColorRepresentation]
-    | [r: number, g: number, b: number]
-    | undefined,
     skyBoxTexture: string[];
 }
 
