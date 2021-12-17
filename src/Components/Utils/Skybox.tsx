24fab69bc48f42aff565bae5aac7c560cd813fe0
Ari Kishon <ariki@wix.com>
2021-12-17 19:36:05 +0200

    fix: fix skybox return value

--- src/Components/Utils/Skybox.tsx
+++ src/Components/Utils/Skybox.tsx
@@ -1,8 +1,11 @@
 import { useThree } from "@react-three/fiber";
-import { CubeTextureLoader } from "three";
+import { ColorRepresentation, CubeTextureLoader } from "three";
 import defaultSkybox from "../../Assets/SkyBoxes/default-skybox/default-skybox";
 
 export interface ISkyboxProps{
+    color: [color: ColorRepresentation]
+    | [r: number, g: number, b: number]
+    | undefined,
     skyBoxTexture: string[];
 }
 
@@ -10,7 +13,8 @@ const SkyBox = ({skyBoxTexture = defaultSkybox}  :ISkyboxProps ) => {
     const { scene } = useThree();
     const loader = new CubeTextureLoader();
     const texture = loader.load(skyBoxTexture);
+    // Set the scene background property to the resulting texture.
     scene.background = texture;
-    return <></>
+    return null
   }
   export default SkyBox;
